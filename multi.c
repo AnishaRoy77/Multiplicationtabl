d15d7e1293e61d2acdc836a90db30078bd7066c4
Anisha Roy <76565991+AnishaRoy77@users.noreply.github.com>
2021-10-04 20:27:23 +0530

    Update multi.c

--- multi.c
+++ multi.c
@@ -3,8 +3,11 @@ int main() {
     int n, i;
     printf("Enter an integer: ");
     scanf("%d", &n);
+    printf("The multiplication table : ");
     for (i = 1; i <= 10; ++i) {
+        
         printf("%d X %d = %d",n,i,n * i);
     }
+    
     return 0;
 }
